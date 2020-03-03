e4746b0fdc12058848ea54b141cff9d5f7f54419
Marine Mekhakyan <marine@spu.local>
2020-03-03 14:34:25 -0800

    video component

---
+++ src/components/video.js
@@ -0,0 +1,28 @@
+import React, { Component, Fragment } from 'react';
+import { render } from '@testing-library/react';
+
+class Video extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            autoplay: true
+        };
+    }
+}
+
+render() {
+    return (
+        <Fragment>
+            <video
+                muted
+                autoplay
+                loop
+                poster='assets/backgroundStill.png' type='video/mp4'>
+                    <source src='assets/backgroundVid.mp4' type='video/mp4' />
+            </video>
+        </Fragment>
+    )
+}
+
+
+export default Video;
