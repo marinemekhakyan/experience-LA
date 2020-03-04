b66ca877dfcd6b4127b4a9063ffcf20683a078eb
Marine Mekhakyan <marine@spu.local>
2020-03-04 14:23:17 -0800

    navbar component

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,13 +1,2 @@
 import React from 'react';
-import classes from './nav.css';
-
-const Nav = () => {
-    return (
-        <div className={classes.Container} >
-            <nav className='mainNav'>
-
-            </nav>
-
-        </div>
-    )
-}
+import classes from './video.css';
