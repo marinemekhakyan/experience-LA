51b384bd831ff14ed260584001560209836a73b9
Marine Mekhakyan <marinemekhakyan@gmail.com>
2020-03-04 21:17:34 -0800

    adding home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import './App.css';
-// import Home from './pages/home';
-import Video from './components/video';
-import Nav from './components/nav'
+import Home from './pages/home';
 
 function App() {
   return (
-    <div>
-    < Nav />
-    < Video />
-    </div>
+   < Home />
   );
 }
 
