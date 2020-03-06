89d0b3c326997686c2b9612d1629ff74a42743f9
Marine Mekhakyan <marinemekhakyan@gmail.com>
2020-03-05 20:50:44 -0800

    adding tripadvisor widget

--- src/components/widget.js
+++ src/components/widget.js
@@ -1,8 +1,16 @@
-<div id="TA_cdswritereviewlgvi169" class="TA_cdswritereviewlgvi">
-<ul id="VMddLCYX" class="TA_links W7mKER">
-<li id="9KDA7TnePgVP" class="PfVqez7VG">
-<a target="_blank" href="https://www.tripadvisor.com/"><img src="https://www.tripadvisor.com/img/cdsi/img2/branding/medium-logo-62975-2.png" alt="TripAdvisor"/></a>
-</li>
-</ul>
-</div>
-<script async src="https://www.jscache.com/wejs?wtype=cdswritereviewlgvi&amp;uniq=169&amp;locationId=20274939&amp;lang=en_US&amp;lang=en_US&amp;display_version=2" data-loadtrk onload="this.loadtrk=true"></script>
+import React from 'react';
+
+
+const Widget = () => {
+    return (
+        <div id="TA_cdswritereviewlgvi169" class="TA_cdswritereviewlgvi">
+            <ul id="VMddLCYX" class="TA_links W7mKER">
+                <li id="9KDA7TnePgVP" class="PfVqez7VG">
+                    <a target="_blank" href="https://www.tripadvisor.com/"><img src="https://www.tripadvisor.com/img/cdsi/img2/branding/medium-logo-62975-2.png" alt="TripAdvisor" /></a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Widget;
