2144d9b8cd15e346123f6fea995473c347751847
Marine Mekhakyan <marinemekhakyan@gmail.com>
2020-03-07 21:03:06 -0800

    adding nooperner to widget

--- src/components/widget.js
+++ src/components/widget.js
@@ -6,7 +6,7 @@ const Widget = () => {
         <div id="TA_cdswritereviewlgvi169" class="TA_cdswritereviewlgvi">
             <ul id="VMddLCYX" class="TA_links W7mKER">
                 <li id="9KDA7TnePgVP" class="PfVqez7VG">
-                    <a target="_blank" href="https://www.tripadvisor.com/"><img src="https://www.tripadvisor.com/img/cdsi/img2/branding/medium-logo-62975-2.png" alt="TripAdvisor" /></a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.tripadvisor.com/"><img src="https://www.tripadvisor.com/img/cdsi/img2/branding/medium-logo-62975-2.png" alt="TripAdvisor" /></a>
                 </li>
             </ul>
         </div>
