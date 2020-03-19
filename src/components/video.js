6659a92211e67a81a0db50dac78a578c05d5a6d9
Marine Mekhakyan <marinemekhakyan@gmail.com>
2020-03-19 11:45:13 -0700

    new video link

--- src/components/video.js
+++ src/components/video.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './main.css';
 
 const Video = () => {
-    const source = 'https://ctala.com/wp-content/uploads/2020/03/drone-video-edit-V3.mp4'
+    const source = 'https://ctala.com/wp-content/uploads/2020/03/drone-video-edit-V3_1.mp4'
     return (
         <div className={classes.Container} >
             <video
